eac8993d383ade8efdd06a7295170324131ffbb8
AlexGol005 <sandra.005@mail.ru>
2022-06-16 21:31:08 +0300

    1

--- scripts/my/main_back.py
+++ scripts/my/main_back.py
@@ -1,5 +1,5 @@
 from p2design import Ui_Form
-from PySide2 import QtWidgets, QtGui
+from PySide2 import QtWidgets, QtGui, QtCore
 
 
 class MyWindow(QtWidgets.QWidget):
@@ -19,6 +19,12 @@ class MyWindow(QtWidgets.QWidget):
         self.ui.pushButtonRT.clicked.connect(self.onPBRTclicked)
         self.ui.pushButtonLD.clicked.connect(self.onPBLDclicked)
         self.ui.pushButtonRD.clicked.connect(self.onPBRDclicked)
+        self.ui.pushButtonCenter.clicked.connect(self.onPBCenterclicked)
+        self.ui.pushButtonGET.clicked.connect(self.getScreenPosition)
+        self.ui.dial.valueChanged.connect(self.showLCD)
+        self.ui.horizontalSlider.valueChanged.connect(self.showLCD)
+        self.ui.dial.installEventFilter(self)
+
 
     def onPBLTclicked(self):
         self.move(0, 0)
@@ -38,8 +44,38 @@ class MyWindow(QtWidgets.QWidget):
         print(self.height())
         self.move(QtWidgets.QApplication.screenAt(self.pos()).size().width() - self.width(), QtWidgets.QApplication.screenAt(self.pos()).size().height() - self.height() - 75)
 
+    def onPBCenterclicked(self):
+        self.move(QtWidgets.QApplication.screenAt(self.pos()).size().width()/2 - self.width()/2, QtWidgets.QApplication.screenAt(self.pos()).size().height()/2 - self.height()/2)
 
+    def getScreenPosition(self):
+        print(self.pos())
+        print(QtWidgets.QApplication.screens())
+        print(QtWidgets.QApplication.primaryScreen())
+        self.ui.plainTextEdit.appendPlainText(str(QtWidgets.QApplication.screens()))
 
+    def showLCD(self):
+        if self.sender().objectName() == "dial":
+            value = self.ui.dial.value()
+            self.ui.horizontalSlider.setValue(value)
+
+        elif self.sender().objectName() == "horizontalSlider":
+            value = self.ui.horizontalSlider.value()
+            self.ui.dial.setValue(value)
+
+# events
+    def changeEvent(self, event: QtCore.QEvent) -> None:
+        if event.type() == QtCore.QEvent.Type.WindowStateChange:
+            if self.isMinimized():
+                print('окно свернуто')
+
+    def moveEvent(self, event: QtGui.QMoveEvent) -> None:
+        print(self.pos())
+
+    def event(self, event: QtCore.QEvent):
+        if event.type() == QtCore.QEvent.Resize:
+            print(self.size())
+
+        return QtWidgets.QWidget.event(self, event)
 
 
 if __name__ == '__main__':
